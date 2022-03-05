5000cb2681185211c79d7e17696b578476c665f7
Logic <63269267+logiqui@users.noreply.github.com>
2022-03-05 06:54:50 -0300

    accept group request

--- start/routes.ts
+++ start/routes.ts
@@ -37,3 +37,4 @@ Route.post('/groups', 'GroupsController.store').middleware('auth')
 
 Route.get('/groups/:groupId/requests', 'GroupRequestsController.index')
 Route.post('/groups/:groupId/requests', 'GroupRequestsController.store').middleware('auth')
+Route.post('/groups/:groupId/requests/:requestId/accept', 'GroupRequestsController.accept')
