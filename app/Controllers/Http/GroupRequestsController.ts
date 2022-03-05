c0049004f1a0cb48e7dfe438579188f529be7d81
Logic <63269267+logiqui@users.noreply.github.com>
2022-03-05 05:57:00 -0300

    groups implementation

---
+++ app/Controllers/Http/GroupRequestsController.ts
@@ -0,0 +1,15 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import GroupRequest from 'App/Models/GroupRequest'
+
+export default class GroupRequestsController {
+  public async store({ request, response, auth }: HttpContextContract) {
+    const groupId = request.param('groupId') as number
+    const userId = auth.user!.id
+
+    const groupRequest = await GroupRequest.create({ groupId, userId })
+    await groupRequest.refresh()
+
+    return response.created({ groupRequest })
+  }
+}
