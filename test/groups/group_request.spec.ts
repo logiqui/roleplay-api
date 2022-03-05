4d667eaf7dce5716ef4c234c53e292cfdddbfc2c
Logic <63269267+logiqui@users.noreply.github.com>
2022-03-05 06:10:38 -0300

    groups request tests

--- test/groups/group_request.spec.ts
+++ test/groups/group_request.spec.ts
@@ -11,7 +11,7 @@ let token = ''
 let user = {} as User
 
 test.group('Group Request', (group) => {
-  test.only('it should create a group request', async (assert) => {
+  test('it should create a group request', async (assert) => {
     const { id } = await UserFactory.create()
     const group = await GroupFactory.merge({ master: id }).create()
 
@@ -25,6 +25,50 @@ test.group('Group Request', (group) => {
     assert.equal(body.groupRequest.userId, user.id)
     assert.equal(body.groupRequest.groupId, group.id)
     assert.equal(body.groupRequest.status, 'PENDING')
+  })
+
+  test('it should return 409 when group request already exists', async (assert) => {
+    const { id } = await UserFactory.create()
+    const group = await GroupFactory.merge({ master: id }).create()
+
+    await supertest(BASE_URL)
+      .post(`/groups/${group.id}/requests`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({})
+
+    const { body } = await supertest(BASE_URL)
+      .post(`/groups/${group.id}/requests`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({})
+      .expect(409)
+
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 409)
+  })
+
+  test.only('it should return 422 when user is already in the group', async (assert) => {
+    const groupPayload = {
+      name: 'test',
+      description: 'test',
+      schedule: 'test',
+      location: 'test',
+      chronic: 'test',
+      master: user.id
+    }
+
+    const response = await supertest(BASE_URL)
+      .post('/groups')
+      .set('Authorization', `Bearer ${token}`)
+      .send(groupPayload)
+
+    const { body } = await supertest(BASE_URL)
+      .post(`/groups/${response.body.group.id}/requests`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({})
+      .expect(422)
+
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
   })
 
   group.before(async () => {
